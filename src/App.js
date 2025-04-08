cd953db548578e53f4ccdd558a7544dc16f68b80
brokendished <brokendished@gmail.com>
2025-04-07 23:14:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.css';
+import Header from './Header';
 import ChatbotChat from './ChatbotChat';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-wrapper">
+      <Header />
       <ChatbotChat />
     </div>
   );
